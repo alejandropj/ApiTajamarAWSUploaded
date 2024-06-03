ad58d76320afadddd1e583d1315e4c93fec42659
alejandropj <alejandroparrajimenez@gmail.com>
2024-06-03 21:36:55 +0200

    03/06

--- ApiTajamarAWS/Models/KeysModel.cs
+++ ApiTajamarAWS/Models/KeysModel.cs
@@ -3,7 +3,10 @@
     public class KeysModel
     {
         public string MySql { get; set; }
-       
+        public string UrlApi { get; set; }
+        public string ApplicationIDSecretName { get; set; }
+        public string SecretKeySecretName { get; set; }
+        public string UrlLogicAppSecretName { get; set; }
+        public string StorageAccountSecretName { get; set; }
     }
-
 }
